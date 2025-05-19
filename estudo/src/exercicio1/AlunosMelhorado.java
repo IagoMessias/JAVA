d30909ec558c38ff248ac023603460e308389db9
IagoMessias <iago.messias013@gmail.com>
2025-05-19 11:05:10 -0300

    adição de "i+1" na mostra de qual aluno

--- estudo/src/exercicio1/AlunosMelhorado.java
+++ estudo/src/exercicio1/AlunosMelhorado.java
@@ -8,14 +8,14 @@ public class AlunosMelhorado {
 		int[] notas = new int[alunos.length];
 		
 		for(int i = 0; i<alunos.length;i++) {
-			System.out.println("Escreva o nome do aluno: ");
+			System.out.println("Escreva o nome do aluno "+(i+1));
 			String nome = entrada.nextLine();
 			alunos[i]=nome;
 			
 		}
 		
 		for (int i =0; i<alunos.length; i++) {
-			System.out.println("Escreva as notas respectivamente: ");
+			System.out.println("Escreva a nota do aluno: "+(i+1));
 			int notasR = entrada.nextInt();
 			notas[i]=notasR;
 			
